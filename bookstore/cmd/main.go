a4e4be387db6994597b35d9c71d4edc4bbc61648
TheKwarantine <kwarantine@gmail.com>
2021-03-31 20:38:51 -0500

    Video 2 : Final Small Bookstore

--- bookstore/cmd/main.go
+++ bookstore/cmd/main.go
@@ -1,23 +1,45 @@
 package main
 
 import "fmt"
+type Book struct {	//Key Value pairs?
+	Title string
+	Author string
+	SeriesNumber int
+	PriceCents int
+	Copies int
+}
+
+var books []Book
+
+func AddBook(catalog []Book, book Book) []Book {
+	catalog = append(catalog, book)
+	return catalog
+}
 
 func main() {
-	title := "The Lost World"
-	author := "Michael Crichton"
-	edition := 1.5
-	inStock := true
-	royaltyPerc := .05
-	onSpecial := false
-	discount := .15
-	copies := 99
 
-	fmt.Println("Title: ", title)
-	fmt.Println("Author: ", author)
-	fmt.Println("Edition: ", edition)
-	fmt.Println("In Stock?: ", inStock)
-	fmt.Println("Royalty Percentage", royaltyPerc)
-	fmt.Println("How many copies?: ", copies)
-	fmt.Println("Is it on special?: ", onSpecial)
-	fmt.Println("Discount Amount: ", discount)
+	
+	books = []Book{
+		{
+			Title: "The Lost World",
+			Author: "Michael Crichton",
+			SeriesNumber: 2,
+			PriceCents: 499,
+			Copies: 1,
+		},
+		{
+			Title: "Jurassic World",
+			Author: "Michael Crichton",
+			SeriesNumber: 1,
+			PriceCents: 499,
+			Copies: 3,
+		},
+	}
+
+	books = AddBook(books, Book{Title: "Spark Joy",Author: "Bill Gates", PriceCents: 1699, Copies: 5})
+
+	for _, b := range books {
+		fmt.Printf("We have %v copies of %s by %s", b.Copies, b.Title, b.Author)
+		fmt.Println()
+	}
 }
